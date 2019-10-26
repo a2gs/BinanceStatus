c96f484902028f2645a9c78fa175268f068ea4d9
Andre Augusto Giannotti Scota <andre.scota@gmail.com>
2019-10-25 22:11:37 -0300

    cleaning history infos

--- binanceStatus.py
+++ binanceStatus.py
@@ -19,8 +19,9 @@ def printOrder(order, seq, tot):
 		+ '\tStop price..: [' + order['stopPrice']      + ']\n'
 		+ '\tIs working..: [' + str(order['isWorking']) + ']')
 
-def printAccount(accBalance, seq, tot):
-	print(f'{seq}/{tot}) Asset balance [' + accBalance['asset'] + '] | Free [' + accBalance['free'] + '] | Locked [' + accBalance['locked'] + ']')
+#def printAccount(accBalance, seq, tot):
+def printAccount(accBalance):
+	print('Asset balance [' + accBalance['asset'] + '] | Free [' + accBalance['free'] + '] | Locked [' + accBalance['locked'] + ']')
 
 try:
 	client = Client(os.getenv('BINANCE_APIKEY', 'NOTDEF_APIKEY'), os.getenv('BINANCE_SEKKEY', 'NOTDEF_APIKEY'), {"verify": True, "timeout": 20})
@@ -30,28 +31,29 @@ try:
 		print('Binance out of service')
 		sys.exit(0)
 
-	print('=1=============================================================================================================')
 	acc = client.get_account()
 	print('Can trade: ' + str(acc['canTrade']) + ' | Can withdraw: ' + str(acc['canWithdraw']) + ' | Can deposit: ' + str(acc['canDeposit']) + ' | Account type: ' + str(acc['accountType']))
 
 	totAccBalance = len(acc['balances'])
 
-	if totAccBalance != 0:
-		[printAccount(n, i, totAccBalance) for i,n in enumerate(acc['balances'], 1) if float(n['free']) != 0.0 or float(n['locked']) != 0.0]
+#	if totAccBalance != 0:
+	if len(acc['balances']) != 0:
+#		[printAccount(n, i, totAccBalance) for i,n in enumerate(acc['balances'], 1) if float(n['free']) != 0.0 or float(n['locked']) != 0.0]
+		[printAccount(n) for n in acc['balances'] if float(n['free']) != 0.0 or float(n['locked']) != 0.0]
 
-	print('=2=============================================================================================================')
-	print(client.get_asset_balance(asset='BTC'))
-	print('=3=============================================================================================================')
-	print(client.get_account_status())
-	print('=4=============================================================================================================')
-	print(client.get_my_trades(symbol='BNBBTC'))
-	print('=5=============================================================================================================')
-	print(client.get_asset_details())
-	print('=6=============================================================================================================')
-	print(client.get_trade_fee())
-	print('=7=============================================================================================================')
-	print(client.get_dust_log())
-	print('=8=============================================================================================================')
+#	print('=2=============================================================================================================')
+#	print(client.get_asset_balance(asset='BTC'))
+#	print('=3=============================================================================================================')
+#	print(client.get_account_status())
+#	print('=4=============================================================================================================')
+#	print(client.get_my_trades(symbol='BNBBTC'))
+#	print('=5=============================================================================================================')
+#	print(client.get_asset_details())
+#	print('=6=============================================================================================================')
+#	print(client.get_trade_fee())
+#	print('=7=============================================================================================================')
+#	print(client.get_dust_log())
+#	print('=8=============================================================================================================')
 
 	# Orders
 	openOrders = client.get_open_orders()
