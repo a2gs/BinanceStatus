936123a6ecf0763793491fd720c7b847c1a26a57
André Augusto Giannotti Scotá <andre.scota@gmail.com>
2020-05-28 00:54:40 -0300

    Update walletId.sh

--- walletId.sh
+++ walletId.sh
@@ -2,3 +2,6 @@
 
 export BINANCE_APIKEY=''
 export BINANCE_SEKKEY=''
+
+# https://github.com/binance-exchange/binance-official-api-docs/blob/master/rest-api.md#timing-security
+export BINANCE_RECVWINDOW=6000
